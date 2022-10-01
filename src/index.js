b24b32ad30af58c3fedd7443c366c20bf3d3ce9d
Leonardo Munarolo <leo_dmun@live.comm>
2022-10-01 15:20:07 -0300

    Exercicio apostila 03 concluido

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './style.css'
+import Medico1 from './img/medico1.jpg'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    const style =() => {
+        return {
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#ffffff',
+            width: '75vh',
+            margin: 'auto',
+            marginTop: '20',
+            borderRadius: '25',
+            padding: '10'
+        }
+    }
+
+    const medicos = [
+        {
+            nome: 'Dra. Julia Ferreira',
+            imagem: Medico1
+        },
+        {
+            nome: 'Dra. Isabel Fernandes',
+            imagem: process.env.PUBLIC_URL + './medico2.jpg'
+        },
+        {
+            nome: 'Dra. Natalia Campos',
+            imagem: 'https://images.pexels.com/photos/5327585/pexels-photo-5327585.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        }
+    ]
+
+    return(
+        <div style={style()}>
+            <h1 className='title'>Profissionais da Saude</h1>
+            <div style={{backgroundColor: '#ddd', display: 'flex', justifyContent: 'space-around', borderRadius: '25px', padding: '10'}}>
+
+                {medicos.map (medico => {
+                    return(
+                        <div className="card">
+                            <div className="card-img">
+                                <img src={medico.imagem} alt="imagemMedico" />
+                            </div>
+                            <p className='card-title'>{medico.nome}</p>
+                        </div>
+                    )
+                })}
+
+            </div>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
